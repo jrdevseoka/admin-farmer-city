ccb3076a52212daa5f5a54230f9f1f0a1d00b844
Dev. GABE <seokamk@gmail.com>
2021-03-16 15:12:21 +0200

    Create Promo

--- src/app/admin/promotions/create-promo/create-promo.component.ts
+++ src/app/admin/promotions/create-promo/create-promo.component.ts
@@ -17,8 +17,9 @@ import { ProductService } from 'src/app/services/product/product.service';
 })
 export class CreatePromoComponent implements OnInit {
   formPromotion: FormGroup;
-
+  productsCollection: AngularFirestoreCollection<Product>;
  products: Observable<Product[]>;
+ product: any;
   constructor(
     private firestore: AngularFirestore,
     private productService: ProductService,
@@ -32,16 +33,58 @@ export class CreatePromoComponent implements OnInit {
         dateStarted: ['', Validators.required],
         dateEnded: ['', Validators.required],
     });
-    this.products = firestore.collection<Product>('products').valueChanges();
+    this.productsCollection = firestore.collection<Product>('products');
+    this.products = this.productsCollection.valueChanges();
     }
     productID: any;
   ngOnInit(){
     //get product ket
      this.productID = this.activated.snapshot.paramMap.get('ref');
      console.log(this.productID);
-    // this.crud.getProduct(this.productID).valueChanges().subscribe(results =>{
-    //   this.products = results;
-    // });
-    console.log(this.products)
+    this.crud.getProduct(this.productID).valueChanges().subscribe(results =>{
+    this.product = results;
+    console.log('Promotion id' + this.product);
+     });
+    // this.products = this.productService.getProducts().snapshotChanges().pipe(
+    //   map(actions => actions.map( e => {
+    //     const data =  e.payload.doc.data() as Product;
+    //     const id = e.payload.doc.id;
+    //     return {...data}
+    //   }))
+    // )
+
+  }
+
+  createPromotion(){
+
+    this.firestore.collection('products').snapshotChanges().subscribe(
+      e => {
+        e.map( results =>{
+          const productinfo = results.payload.doc.data() as Product;
+          console.log(this.product.productPrice);
+          const rate = Number(this.formPromotion.get('percentageOff')?.value)/100
+          let price =  Number(productinfo.productPrice);
+
+          const promotionPrice = rate * Number(productinfo.productPrice);
+          console.log('Promotion rate is '+ rate)
+          console.log('Promotion Price is '+ promotionPrice);
+          //Calculating new product Price
+          price = price - promotionPrice;
+
+          let newPrice : any  = {
+            'productPrice' : price
+          }
+
+
+          console.log('New item price ' +price);
+          this.firestore.collection('products').doc(this.productID).update(newPrice).then( res => {
+            console.log(res)
+          }).catch(err => {
+            console.log(err.message)
+          })
+
+        })
+      }
+    )
   }
 }
